4ff4e0ba70a0cb8edbf4ceb3466540523f8e2db4
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-08-02 00:20:01 -0500

    creando tareas

--- src/addTask.js
+++ src/addTask.js
@@ -4,19 +4,31 @@ const AWS = require('aws-sdk');
 const addTask = async(event) => {
     const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const {title, description} = JSON.parse(event.body);
-  const createdAt = new Date();
-  const id = v4();
+    const {title, description} = JSON.parse(event.body);
+    const createdAt = new Date();
+    const id = v4();
 
-  const newTask = {
+    const newTask = {
     id,
     title,
     description,
-    createdAt
-  }
+    createdAt,
+    done: false,
+    }
 
-/*   const newTask = {
-    tableName: 'TaskTable',
+    await dynamodb.put({
+    TableName: 'TaskTable',
+    Item: newTask
+    }).promise()
+
+    return {
+    statusCode: 200,
+    body: JSON.stringify(newTask)
+    }  
+
+
+/*    const newTask = {
+    TableName: 'TaskTable',
     Item: {
         id,
         title,
@@ -26,23 +38,25 @@ const addTask = async(event) => {
  
   };
 
+let output = "";
+
   dynamodb.put( newTask, (error, data) => {
    if(error) {
     console.log("Unable to save data, error: ", JSON.stringify(error, null, 2));
+    output = JSON.stringify(error, null, 2);
    } else{
     console.log("saving data:: ", JSON.stringify(data, null, 2));
+    output = JSON.stringify(data, null, 2);
    }
-  }); */
-
-    await dynamodb.put({
-    TableName: 'TaskTable',
-    Item: newTask
-  }).promise()
+  });  
 
   return {
-    statusCode: 200,
+    statusCode: 202,
+    output: output,
     body: JSON.stringify(newTask)
-  } 
+  } */
+
+ 
 }
 
 
